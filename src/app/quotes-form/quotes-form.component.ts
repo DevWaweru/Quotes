149eb705646d6d5f2a7ad8f6ede4b5c669986da7
Waweru <developer.waweru@gmail.com>
2018-04-20 16:54:07 +0300

    Editing the html form component

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 
+  newQuoteForm = new Quotes(0,"","");
+  @Output() addNewQuote = new EventEmitter<Quotes>();
+
+  submitQuote(newOne){
+    this.addNewQuote.emit(newOne.value);
+  }
   constructor() { }
 
   ngOnInit() {
